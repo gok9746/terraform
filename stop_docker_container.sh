77eb82d27b51f388dc1bdaa6952aa2f30572caf4
gok9746 <gokulsaji2016@gmail.com>
2025-03-06 10:38:34 +0000

    Delete stop_docker_container.sh

--- stop_docker_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set container name
-container_name="web-app-container"
-
-# Check if the container is running and stop it
-docker ps -q -f name=$container_name | grep -q . && docker stop $container_name
-docker ps -q -f name=$container_name | grep -q . && docker rm $container_name
